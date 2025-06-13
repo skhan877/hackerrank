524d5de26da71806e37b07ac6cb4de40bac9fb4c
skhan59bbk <skhan59@dcs.bbk.ac.uk>
2025-06-13 21:12:18 +0100

    more solutions

--- sandbox.py
+++ sandbox.py
@@ -138,6 +138,11 @@ def diag_diff(n, M):
         b += M[n-1-i][i]
     return abs(a - b)
 
+def counting_sort(a): 
+    c = [0] * 100
+    for x in a: 
+        c[x] += 1 
+    return c 
 
 def main(): 
     # print(towers(2, 2))
@@ -160,9 +165,11 @@ def main():
     # print(time_conversion("12:18:25AM"))
     # print(lonely([1,2,3,4,3,2,1]))
     # print(lonely([1,1,1,3,5,5,5,3]))
-    print(diag_diff(3, [[11,2,4], [4,5,6], [10,8,-12]]))
-    print(diag_diff(3, [[1,2,3], [4,5,6], [9,8,9]]))
-
+    # print(diag_diff(3, [[11,2,4], [4,5,6], [10,8,-12]]))
+    # print(diag_diff(3, [[1,2,3], [4,5,6], [9,8,9]]))
+    # arr = "63 25 73 1 98 73 56 84 86 57 16 83 8 25 81 56 9 53 98 67 99 12 83 89 80 91 39 86 76 85 74 39 25 90 59 10 94 32 44 3 89 30 27 79 46 96 27 32 18 21 92 69 81 40 40 34 68 78 24 87 42 69 23 41 78 22 6 90 99 89 50 30 20 1 43 3 70 95 33 46 44 9 69 48 33 60 65 16 82 67 61 32 21 79 75 75 13 87 70 33 "
+    # print(counting_sort([1,1,3,2,1]))
+    # print(counting_sort(list(map(int, arr.split()))))
 
 if __name__ == "__main__": 
     main() 
